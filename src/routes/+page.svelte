2b2a035ee7ca009e3ed24341e75f09ab19b77493
Federico Balzi <federico@joinbubble.com>
2023-02-08 23:21:22 +0000

    fix(CI): prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <div class="container">
-  <h1 class="content">Hello, World!</h1>
+	<h1 class="content">Hello, World!</h1>
 </div>
 
 <style>
-  .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: calc(100vh - 32px);
-  }
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: calc(100vh - 32px);
+	}
 
-  .container {
-    color: blueviolet;
-  }
+	.container {
+		color: blueviolet;
+	}
 </style>
