24fbcd13434abbafbb96ee705f37cba02d2e5684
Federico Balzi <federico@joinbubble.com>
2023-02-08 23:08:33 +0000

    feat(aws-web): project init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div class="container">
+  <h1 class="content">Hello, World!</h1>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 32px);
+  }
+
+  .container {
+    color: blueviolet;
+  }
+</style>
