86b5ee6bf49a993bfad71dda8cb9ba4ffab325b1
Federico Balzi <88090587+federico-pi@users.noreply.github.com>
2023-02-12 13:53:13 +0000

    fix(svelte-config): cd (#7)
    Co-authored-by: Federico Balzi <federico@joinbubble.com>

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 export default {
@@ -6,6 +6,15 @@ export default {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true,
+			fallback: '200.html'
+		}),
+		prerender: { entries: [] }
 	}
 };
