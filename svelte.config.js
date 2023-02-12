dd2c82921dcde38f11000828fc89b7e152326eb0
Federico Balzi <federico@joinbubble.com>
2023-02-12 13:39:37 +0000

    fix(CD): svelte config update

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		})
+		adapter: adapter()
 	}
 };
-
-export default config;
